cabc51be2d3b2c5746897c7e0c4da78634fc525b
Liu-Xuan <445221708@qq.com>
2025-01-26 14:05:50 +0800

    清空主分支

--- extension_old2/popup/render.js
+++
@@ -1,8 +0,0 @@
-// React 组件渲染
-const container = document.getElementById('document-list');
-ReactDOM.render(
-    React.createElement('div', { className: 'app-container' },
-        React.createElement(DocumentList)
-    ),
-    container
-); 
